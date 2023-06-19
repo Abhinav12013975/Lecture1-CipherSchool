7a94ed94e722e5413ed7521074716db4ba4c6167
Abhinav12013975 <89858848+Abhinav12013975@users.noreply.github.com>
2023-06-19 14:44:11 +0530

    Add files via upload

---
+++ Lecture_8_cipherSchool.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+// void printstuff()
+// {
+//     cout<<"writing my first function";
+// }
+int sum (int a, int b)
+{
+    int c;
+    c=a+b;
+    return c;
+}
+int main()
+{
+    // printstuff();
+    int a=3;
+    int b=5;
+    int s;
+   s =  sum(a,b);
+}
